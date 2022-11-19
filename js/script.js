55767af98786d6f6a7b1c7f225060ce318d6c56f
EyasuLingerih <elingerih@gmail.com>
2022-11-19 15:16:39 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,23 +4,10 @@
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
+
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
+    
 });
 
 // Set the date we're counting down to
@@ -85,9 +72,3 @@ var styles2 = [
 
 console.log('\n\n%c SAVE THE DATE: 07th Jan, 2023!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
