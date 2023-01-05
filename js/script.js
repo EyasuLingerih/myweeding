c52eab5c31fd3fa7b51b4b686c080b91998185f8
Eyasu Lingerih <elingerih@gmail.com>
2023-01-05 21:53:20 +0300

    Change hour value

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("JAN 07 , 2023 11:00:00").getTime();
+var countDownDate = new Date("JAN 07 , 2023 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
